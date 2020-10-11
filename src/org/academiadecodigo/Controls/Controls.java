0e3543b2d9eb7c078e7fc7b2ffeebd8787c31b47
Pedro Rafael <pmdr93@gmail.com>
2020-10-11 18:12:48 +0100

    begin Controls

--- src/org/academiadecodigo/Controls/Controls.java
+++ src/org/academiadecodigo/Controls/Controls.java
@@ -1,6 +1,28 @@
 package org.academiadecodigo.Controls;
 
-public class Controls {
+import org.academiadecodigo.Objects.PacMan;
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
+public class Controls implements KeyboardHandler{
+
+    private Keyboard keyboard;
+    private PacMan pacman;
+
+    public void init() {
+        keyboard = new Keyboard(this);
+    }
 
 
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+
+
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+
+    }
 }
--- src/org/academiadecodigo/Objects/Characters.java
+++ src/org/academiadecodigo/Objects/Characters.java
@@ -2,7 +2,7 @@ package org.academiadecodigo.Objects;
 
 public class Characters extends GameObjects{
 
-    private int numberLives;
+    private int numberLives = 3;
     private boolean dead;
     private boolean resetCovid;
 
