9ea5b64cbeea9276b4c5f98d6c24c19b615c738b
Pedro Rafael <pmdr93@gmail.com>
2020-10-18 11:34:54 +0100

    pacMan only dies with the first covid created e kill only them

--- src/org/academiadecodigo/MakeWords/MakeWords.java
+++ src/org/academiadecodigo/MakeWords/MakeWords.java
@@ -12,14 +12,14 @@ public class MakeWords {
 
     private Picture[] codigo = new Picture[] {
 
-            new Picture( 890,52,"resources/Letters/letterA.png"),
-            new Picture(40,200,"resources/Letters/letterC.png"),
+            new Picture( 935,75,"resources/Letters/letterA.png"),
+            new Picture(60,200,"resources/Letters/letterC.png"),
             new Picture(835,450,"resources/Letters/letterA.png"),
-            new Picture(360,498,"resources/Letters/letterD.png"),
-            new Picture(690,125,"resources/Letters/letterE.png"),
+            new Picture(360,500,"resources/Letters/letterD.png"),
+            new Picture(685,125,"resources/Letters/letterE.png"),
             new Picture(285,100,"resources/Letters/letterM.png"),
-            new Picture(490,250,"resources/Letters/letterI.png"),
-            new Picture(12,475,"resources/Letters/letterA.png"),
+            new Picture(510,250,"resources/Letters/letterI.png"),
+            new Picture(35,475,"resources/Letters/letterA.png"),
     };
 
     private Picture[] codigoDown = new Picture[] {
